32e169a4e2684d8a2d885845bb7e76dcca616ec5
Senpumaru18 <stromtiger18@gmail.com>
2023-01-11 14:49:28 -0600

    class

---
+++ HelloPrinter.java
@@ -0,0 +1,14 @@
+public class HelloPrinter
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello, World!");
+        System.out.println("3+4");
+        System.out.println(3+4);
+
+        /*Errors
+         * System.ou.print("Hello, World!"); Compliler error
+         * System.out.print("Hello, Word!") Syntax error
+        */
+    }
+}
